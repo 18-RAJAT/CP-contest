59b255c75fd3b6dcfa8b72eead650c55b0f08d66
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-06-02 08:33:26 +0530

    02/06

---
+++ B_Marin_and_Anti-coprime_Permutation.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nL
+
+
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int ttt; cin >> ttt;
+long long mod = 998244353;
+while(ttt--) {
+//_______________________________________________________//
+Rajat_Joshi CSE;
+	ll n,ans=1;
+		cin>>n;
+		if(n%2==1) cout<<0<<endl;
+		else{
+			n=n/2;
+			for(ll i=1;i<=n;i++){
+				ans=ans*i%mod;
+				ans%=mod;
+			}
+			cout<<(ans%mod)*(ans%mod)%mod<<endl;
+        }
+}
+return 0;
+}
