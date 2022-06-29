2d0f1dd7c2a1f726f26e0dad206cb4550bcedff8
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-06-29 19:28:42 +0530

    27/06

---
+++ A_Boy_or_Girl.cpp
@@ -0,0 +1,320 @@
+/*
+* Note: This template uses some c++11 functions , so you have to compile it with c++11 flag.
+*       Example:-   $ g++ -std=c++11 c++Template.cpp
+*
+* Author : Rajat Joshi
+* Handle: 18-RAJAT
+*
+*/
+
+// Those who cannot remember the past are condemned to repeat it
+
+ 
+/********   All Required Header Files ********/
+
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*******  All Required define Pre-Processors and typedef Constants *******/
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+// ----------------------<MATH>--------------------------- 
+ 
+template<typename T> T gcd(T a, T b){return(b?__gcd(a,b):a);} 
+
+template<typename T> T lcm(T a, T b){return(a*(b/gcd(a,b)));} 
+
+int add(int a, int b, int c = MOD){int res=a+b;
+                         return(res>=c?res-c:res);} 
+int mod_neg(int a, int b, int c = MOD){int res;
+                         if(abs(a-b)<c)res=a-b;else res=(a-b)%c;
+                         return(res<0?res+c:res);} 
+int mul(int a, int b, int c = MOD){ll res=(ll)a*b;
+                         return(res>=c?res%c:res);} 
+int muln(int a, int b, int c = MOD){ll res=(ll)a*b;}
+  
+// // Permutation and Combination
+// int ncr(int n,int r,int c = MOD){ 
+//     return mul(mul(ifact[r],ifact[n-r],c),fact[n],c); 
+// }  
+
+// ----------------------</MATH>-------------------------- 
+
+
+
+
+
+// ----------------------</BITWISE>-------------------------- 
+/* a=target variable, b=bit number to act upon 0-n */
+#define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+
+// '!!' to make sure this returns 0 or 1
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b))))
+
+#define BITMASK_SET(x, mask) ((x) |= (mask))
+#define BITMASK_CLEAR(x, mask) ((x) &= (~(mask)))
+#define BITMASK_FLIP(x, mask) ((x) ^= (mask))
+#define BITMASK_CHECK_ALL(x, mask) (!(~(x) & (mask)))
+#define BITMASK_CHECK_ANY(x, mask) ((x) & (mask))
+// ----------------------</BITWISE END>--------------------------
+
+
+
+
+
+
+/****************** Prime Generator **********************/ 
+const int N=1e7+10; int prime[20000010]; 
+bool isprime[N]; int nprime; 
+template <typename T> void Sieve(T a) 
+{nprime = 0;memset(isprime,true,sizeof(isprime));
+isprime[1]=false;for(int i=2;i<N;i++){
+if(isprime[i]){prime[nprime++]=i;for(int j=2;i*j<N;j++)
+isprime[i*j]=false;}}}
+template <typename T> inline T PrimeFactors(T n)
+{ll cnt=0,sum=1;for(int i=0; prime[i]*prime[i]<=n 
+and i<nprime;i++){cnt=0;while(n%prime[i]==0)
+{cnt++;n/=prime[i];}sum*=(cnt+1);}
+if(n>1)sum*=2;return sum;} 
+/****************** Prime Generator End **********************/ 
+
+
+
+
+
+
+/****************** Geometry *****************/ 
+template <typename T> inline T PointDistanceHorVer(T x1,T y1,T x2, T y2) 
+{return abs(x1-x2)+abs(y1-y2);} 
+template <typename T> inline T PointDistanceDiagonally(T x1,T y1,T x2, T y2) 
+{return sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));} 
+template <typename T> inline T PointDistanceMinimum(T x1,T y1,T x2, T y2) 
+{ T tmp1=abs(x1-x2); T tmp2=abs(y1-y2); T tmp3=abs(tmp1-tmp2); 
+T tmp4=min(tmp1, tmp2); return tmp3+tmp4; } 
+template <typename T> inline T PointDistance3D(T x1,T y1,T z1,T x2,T y2,T z2)
+{return sqrt(square(x2-x1)+square(y2-y1)+square(z2-z1));} 
+ 
+template <typename T> inline T Cube(T a){return a*a*a;} 
+template <typename T> inline T RectengularPrism(T a,T b,T c)
+{return a*b*c;} 
+template <typename T> inline T Pyramid(T base, T height)
+{return (1/3)*base*height;}
+template <typename T> inline T Ellipsoid(T r1,T r2,T r3) 
+{return (4/3)*PI*r1*r2*r3;} 
+template <typename T> inline T IrregualarPrism(T base, T height)
+{return base*height;} 
+template <typename T> inline T Sphere(T radius)
+{ return (4/3)*PI*radius*radius*radius;} 
+template <typename T> inline T CylinderB(T base, T height)
+{return base*height;} // base and height 
+template <typename T> inline T CylinderR(T radius, T height)
+{return PI*radius*radius*height;} // radius and height 
+template <typename T> inline T Cone (T radius,T base, T height)
+{return (1/3)*PI*radius*radius*height;} 
+/****************** Geometry end *****************/ 
+
+
+
+
+
+
+
+/****** Template of some basic operations *****/
+template<typename T, typename U> inline void amin(T &x, U y) { if(y < x) x = y; }
+template<typename T, typename U> inline void amax(T &x, U y) { if(x < y) x = y; }
+/**********************************************/
+
+/****** Template of Fast I/O Methods *********/
+template <typename T> inline void write(T x)
+{
+    int i = 20;
+    char buf[21];
+    // buf[10] = 0;
+    buf[20] = '\n';
+
+    do
+    {
+        buf[--i] = x % 10 + '0';
+        x/= 10;
+    }while(x);
+    do
+    {
+        putchar(buf[i]);
+    } while (buf[i++] != '\n');
+}
+
+template <typename T> inline T readInt()
+{
+    T n=0,s=1;
+    char p=getchar();
+    if(p=='-')
+        s=-1;
+    while((p<'0'||p>'9')&&p!=EOF&&p!='-')
+        p=getchar();
+    if(p=='-')
+        s=-1,p=getchar();
+    while(p>='0'&&p<='9') {
+        n = (n<< 3) + (n<< 1) + (p - '0');
+        p=getchar();
+    }
+
+    return n*s;
+}
+/************************************/
+
+
+/******* Debugging Class Template *******/
+#define DEBUG
+
+#ifdef DEBUG
+
+    #define debug(args...)     (Debugger()) , args
+
+    class Debugger
+    {
+        public:
+        Debugger(const std::string& _separator = " - ") :
+        first(true), separator(_separator){}
+
+        template<typename ObjectType> Debugger& operator , (const ObjectType& v)
+        {
+            if(!first)
+                std:cerr << separator;
+            std::cerr << v;
+            first = false;
+            return *this;
+        }
+        ~Debugger() {  std:cerr << endl;}
+
+        private:
+        bool first;
+        std::string separator;
+    };
+
+#else
+    #define debug(args...)                  // Just strip off all debug tokens
+#endif
+
+/**************************************/
+
+/******** User-defined Function *******/
+
+
+/**************************************/
+
+
+/********** Main()  function **********/
+
+// void Fpresuf(int pre[],int a[],int suf[],int n)
+// {
+//     pre[0]=a[0];
+//     for(int i=1;i<n;++i)
+//         pre[i]=__gcd(pre[i-1],a[i]);
+//     suf[n-1]=a[n-1];
+
+//     for(int i=n-2;i>=0;--i)
+//         suf[i]==__gcd(suf[i+1],a[i]);
+// }
+
+// int gcdR(int l,int r,int pre[],int suf[],int n)
+// {
+//     if(l==0)return suf[r+1];
+//     if(r==n-1)return pre[l-1];
+//     return __gcd(pre[l-1],suf[r+1]);
+// }
+
+
+int n,s;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+#endif
+    // int tc=1;
+    // tc = read(int);
+    //     while(tc--)
+    // {
+        
+    // }   
+        string userName;cin>>userName;
+        int count=0;
+        for(int i=0;i<userName.length()-1;++i)
+        {
+            for(int j=i+1;j<userName.length();++j)
+            {
+                if(userName[i]==userName[j])
+                    {count++;break;}
+            }
+        }
+        int answer=userName.length()-count;
+        if(answer%2==0){cout<<"CHAT WITH HER!"<<"\n";}
+        else{cout<<"IGNORE HIM!"<<"\n";}
+    return 0;
+}
--- B_Promo.cpp
+++ B_Promo.cpp
@@ -1,53 +1,77 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nL
+const int N = 2e5+10;
 
-void bfs_traversal(vector<int>arr[],int v)
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
 {
-    vector<int> bfsStore;
-    vector<int> vis(0,v+1);
-    queue<int> q;
-    q.push(0);
-    vis[0] = 1;
-    while(!q.empty())
-    {
-        int node = q.front();
-        q.pop();
-        bfsStore.push_back(node);
-        for(auto it: arr[node])
-        {
-            if(!vis[it])
-            {
-                q.push(it);
-                vis[it] = 1;
-            }
-        }
-    }cout<<"hello";
-    for(int i=0;i<bfsStore.size();i++)
-        cout<<bfsStore[i]<<" ";
-    cout<<endl;
-
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+// int ttt; cin >> ttt;
+// while(ttt--) {
+//_______________________________________________________//
+Rajat_Joshi CSE;
+   int n,q;
+	cin >> n >> q;
+	long long a[n+1],b[n+1];
+	b[0]=0;
+	for (int i=1; i<=n; i++){
+		cin >> a[i];
+	}
+	sort(a+1,a+n+1);
+	reverse(a+1,a+n+1);
+	for (int i=1; i<=n; i++){
+		b[i]=b[i-1]+a[i];
+	}
+	long long x,y;
+	for (int i=0; i<q; i++){
+		cin >> x >> y;
+		cout << b[x]-b[x-y] <<"\n"; 
+	}
+return 0;
 }
 
-int main()
-{
-    int v,e;
-    cin>>v>>e;
-    vector<int> arr[v+1];
-    for(int i=0;i<e;++i)
-    {
-        int u,t;
-        cin>>u>>t;
-        arr[u].push_back(t);
-		arr[t].push_back(u);
-    }
-    bfs_traversal(arr,v);
-    return 0;
-}
+
+
