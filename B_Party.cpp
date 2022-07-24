9ef9c6f2788134ae6b98d8f1abd04b9510ca6f34
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-07-24 22:58:36 +0530

    24/07

--- B_Party.cpp
+++ B_Party.cpp
@@ -1,84 +1,139 @@
-#include<bits/stdc++.h>
-#define Rajat_Joshi            main;
-#define  CSE             ios_base::sync_with_stdio(false);
-#define Joshi           cin.tie(NULL);
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <sstream>
-#include <queue>
-#include <deque>
-#include <bitset>
-#include <iterator>
-#include <list>
-#include <stack>
-#include <map>
-#include <set>
-#include <functional>
-#include <numeric>
-#include <utility>
-#include <limits>
-#include <time.h>
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define read(type) readInt<type>()
+
+#define ar array
 #define ll long long
-#define nL
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
 
-
-#define pb push_back
-#define mk make_pair
-#define pii pair<int, int>
-#define a first
-#define b second
-#define vi vector<int>
-#define all(x) (x).begin(), (x).end()
-#define umap unordered_map
-#define uset unordered_set
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+#define SCD(t) scanf("%d",&t)
+#define SCLD(t) scanf("%ld",&t)
+#define SCLLD(t) scanf("%lld",&t)
+#define SCC(t) scanf("%c",&t)
+#define SCS(t) scanf("%s",t)
+#define SCF(t) scanf("%f",&t)
+#define SCLF(t) scanf("%lf",&t)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define FOR(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define RFOR(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define rall(cont) cont.end(), cont.begin()
+#define FOREACH(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define IN(A, B, C) assert( B <= A && A <= C)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
 #define MOD 1000000007
-#define imax INT_MAX
-#define imin INT_MIN
-#define exp 1e9
-#define sz(x) (int((x).size()))
-void solve()
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+const double pi=acos(-1.0);
+#define ll long long
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<vector<int>> VII;
+typedef vector<string> VS;
+typedef vector<PII> VIII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+#define ff(a,b) for(int i=a;i<b;i++)
+
+/* clang-format off */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define mii map<int,int>
+#define si set<int>
+#define sc set<char>
+#define vi vector<int>
+
+/* FUNCTIONS */
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+
+
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+
+
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
+
+
+
+void solve() 
 {
-    int N;
-    cin >> N;
-    vector<int> A;
-    int even = 2, odd = 1;
-    if(N==1) {cout<<1<<endl;return;}
-    for (int i = 0; i < N; i++)
+    int n,m;cin>>n>>m;int a[n];
+    vvi v(n+1);
+    vi visited(n+1,0);
+    f1(i,0,n){cin>>a[i];}
+
+    int ct=m,minimum=INT_MAX;
+    while(m--)
     {
-        if (i % 2 == 0)
-        {
-            A.pb(even);
-            even += 2;
-        }
-        else
-        {
-            A.pb(odd);
-            odd += 2;
-        }
+        int x,y;cin>>x>>y;
+        v[x].pb(y);
+        v[y].pb(x);
+
+        minimum=min(minimum,a[x-1]+a[y-1]);
     }
-    if(N%2) swap(A[N-1],A[N-2]);
-    for (auto i : A)
-        cout << i << " ";
-    cout << endl;
+    if(ct%2==0){cout<<0<<"\n";}
+    cf(i,1,n)
+    {
+        if(v[i].size()%2==1)
+            {minimum=min(minimum,a[i-1]);}
+    }
+    cout<<minimum<<"\n";
 }
-int32_t main()
-{
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-int ttt; cin >> ttt;
-while(ttt--) {
-//_______________________________________________________//
-Rajat_Joshi CSE;
-solve();
-}
-return 0;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+#endif
+    int tc = 1;
+    cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
 }
