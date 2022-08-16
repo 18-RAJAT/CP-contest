08460cca05a97f40cce3e1dd75f2ea68616ba8bc
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-16 09:41:04 +0530

    14/08

---
+++ B_Bad_Ugly_Numbers.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+#define Rajat_Joshi            main;
+#define  CSE             ios_base::sync_with_stdio(false);
+#define Joshi           cin.tie(NULL);
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+#define read(type) readInt<type>()
+#define ll long long
+#define nl
+
+#define f1(i,s,e) for(long long int i=s;i<e;i++)
+#define ff1(i,s,e) for(long long int i=s;i>=e;--i)
+#define For1(i,n) or(long long int i=0;i<n+1;++i)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define FO(i,s,e) for(long long int i=1;i*i<=y;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pass(a)  for(long long int i=n-1;i>=1;i-=2)
+#define pb push_back
+#define eb emplace_back
+#define pb push_back
+#define mk make_pair
+#define pii pair<int, int>
+#define a first
+#define b second
+#define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define umap unordered_map
+#define uset unordered_set
+#define MOD 1000000007
+#define imax INT_MAX
+#define imin INT_MIN
+#define exp 1e9
+#define sz(x) (int((x).size()))
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+int ttt; cin >> ttt;
+while(ttt--) {
+//_______________________________________________________//
+Rajat_Joshi CSE;
+int n;cin>>n;if(n==1){cout<<"-1"<<endl;}
+    else
+    {
+        f1(i,0,n-1)
+        cout<<"3";cout<<"7"<<endl;
+    }
+}
+return 0;
+}
