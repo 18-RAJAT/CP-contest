74e766ca06526192f29f0250e0eac8bbeaca5448
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-09-14 18:49:01 +0530

    14/09

--- A_Sinking_Ship.cpp
+++ A_Sinking_Ship.cpp
@@ -268,10 +268,10 @@ void solve()
 int main() {
 ios_base::sync_with_stdio(0);
 cin.tie(0); cout.tie(0);
-// #ifndef ONLINE_JUDGE
-// freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
-// freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
-// #endif
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+#endif
     // ll tc;
     // cin >> tc;
     // for (int t = 1; t <= tc; t++) {
