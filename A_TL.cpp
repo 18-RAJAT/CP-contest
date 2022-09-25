fd3f00b26aa28f0b8d7d008cba527ab953099592
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-09-25 18:06:43 +0530

    25/09

--- A_TL.cpp
+++ A_TL.cpp
@@ -397,7 +397,7 @@ bool c1(pair<string,pair<int,int>> &x,pair<string,pair<int,int>>&y)
 
 }
 
-int a[1001],b[1001];
+int a[101],b[101];
 void solve()
 {
     int n,m;
@@ -426,4 +426,4 @@ int main() {
         solve();
     // }
     return 0;
-}
+}   
