ba129fddcbf0b386c65bf4f97e69a0daf365f648
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-09-26 20:08:36 +0530

    26/09

--- C_Minimum_Notation.cpp
+++ C_Minimum_Notation.cpp
@@ -400,7 +400,7 @@ bool c1(pair<string,pair<int,int>> &x,pair<string,pair<int,int>>&y)
 void solve()
 {
     
-string s;
+    string s;
         cin>>s;
         int n=s.size();
         reverse(all(s));
