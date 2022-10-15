ee703fa550609a77e13292a58f6fe0463e2c52a6
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-10-15 10:23:28 +0530

    15/10

--- E_Scuza.cpp
+++ E_Scuza.cpp
@@ -355,10 +355,10 @@ void solve()
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
-#ifndef ONLINE_JUDGE
-freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
-freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
-#endif
+// #ifndef ONLINE_JUDGE
+// freopen("input.txt","r",stdin); //file input.txt is opened in reading mode i.e "r"
+// freopen("output.txt","w",stdout);  //file output.txt is opened in writing mode i.e "w"
+// #endif
     ll tc = 1;
     cin >> tc;
     for (ll t = 1; t <= tc; t++) {
