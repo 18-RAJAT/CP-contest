b1fdf61ead5dff48423d595f731394d6f1c7c88b
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-04-18 06:42:40 +0530

    18/04/23

--- B_Codeforces_Subsequences.cpp
+++ B_Codeforces_Subsequences.cpp
@@ -3,12 +3,36 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin>>n;
-    string str="codeforces";
-    for(int i=1;i<n;i++)
+    long long int n;cin>>n;
+    vector<long long int>array(10,1);
+    long long int current=1;
+    long long int index=0;
+
+    while(current<n)
     {
-        str+="s";
+        current/=array[index];
+        array[index]++;
+        current*=array[index];
+        index=(index+1)%10;
     }
-    cout<<str;
+
+    auto printAllPossible=[&](char character,long long int numberOfTime)->auto
+    {
+        for(long long int i=0;i<numberOfTime;++i)
+        {
+            cout<<character;
+        }
+    };
+    printAllPossible('c',array[0]);
+    printAllPossible('o',array[1]);
+    printAllPossible('d',array[2]);
+    printAllPossible('e',array[3]);
+    printAllPossible('f',array[4]);
+    printAllPossible('o',array[5]);
+    printAllPossible('r',array[6]);
+    printAllPossible('c',array[7]);
+    printAllPossible('e',array[8]);
+    printAllPossible('s',array[9]);
+
+    return 0;
 }
