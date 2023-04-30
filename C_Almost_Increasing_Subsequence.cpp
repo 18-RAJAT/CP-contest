58fb4a71702b9c8d2eb8ac5eefaa5c74c432c604
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-04-30 10:55:07 +0530

    30/04/23

--- C_Almost_Increasing_Subsequence.cpp
+++ C_Almost_Increasing_Subsequence.cpp
@@ -1,106 +1,81 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int arr[200001];
-int prefix[200001];
-int suffix[200001];
+int arr[200005];
+int prefix[200005]={0},suffix[200005];
+
 void sol()
 {
     int n,q;
     cin>>n>>q;
-    // vector<int>arr(200001);
-    // memset(arr,0,sizeof(arr));
-
     for(int i=1;i<=n;++i)
     {
         cin>>arr[i];
     }
-    // int countLongestIncSubs=0;
-    // vector<int>prefix(200001,0);
-    // vector<int>suffix(200001);
-    memset(prefix,0,sizeof(prefix));
-
-    // prefix[1]=1,prefix[2]=2;
     for(int i=2;i<=n-1;++i)
     {
-        // prefix[i]=prefix[i-1];
-        // if(arr[i]>arr[i-1])
-        // {
-        //     prefix[i]=prefix[i-1]+1;
-        // }
-        // if(not (arr[i-1]<arr[i] and arr[i]<arr[i+1]))
-
-        // if(not(arr[i-2]>=arr[i-1] and arr[i-1]>=arr[i]))
-        // {
-        //     // countLongestIncSubs+=1;
-        //     prefix[i]+=1;
-        // }
-        if(arr[i]<=arr[i-1] and arr[i+1]<=arr[i])
+        if(arr[i-1]>=arr[i] and arr[i]>=arr[i+1])
         {
             prefix[i]=prefix[i-1]+1;
+            // suffix[i]=suffix[i+1]+1;
             suffix[i]=1;
         }
         else
         {
             prefix[i]=prefix[i-1];
         }
-        // else
-        // {
-        //     prefix[i]=2;
-        // }
     }
     prefix[n]=prefix[n-1];
+    // for(int i=n-1;i>=1;--i)
+    // {
+    //     if(arr[i+1]>=arr[i] and arr[i]>=arr[i-1])
+    //     {
+    //         suffix[i]=suffix[i+1]+1;
+    //     }
+    //     else
+    //     {
+    //         suffix[i]=suffix[i+1];
+    //     }
+    // }
+    // suffix[1]=suffix[2];
     int left,right;
-    // while(q--)
     for(int i=1;i<=q;++i)
     {
         cin>>left>>right;
-        // if(right-left+1<3)
+        // if(l==r)
         // {
-        //     cout<<right-left+1<<"\n";
-        //     // continue;
+        //     cout<<"0"<<"\n";
+        //     continue;
         // }
+        // if(l+1==r)
+        // {
+        //     cout<<"1"<<"\n";
+        //     continue;
+        // }
+        // if(arr[l]>=arr[l+1] and arr[l]>=arr[l-1])
+        // {
+        //     cout<<r-l+1-max((prefix[r]-prefix[l]),0)<<"\n";
+        //     continue;
+        // }
+        // if(arr[r]>=arr[r+1] and arr[r]>=arr[r-1])
+        // {
+        //     cout<<r-l+1-max((suffix[l]-suffix[r]),0)<<"\n";
+        //     continue;
+        // }
+        // cout<<r-l+1-max((prefix[r]-prefix[l]),0)-max((suffix[l]-suffix[r]),0)<<"\n";
+
         if(suffix[right]==1)
         {
-            // int maximum=max<int>(prefix[right]-prefix[right]-1,0);
-            // cout<<right-left+1-maximum<<"\n";
-            cout<<right-left+1-max((prefix[right]-prefix[right-1]),0)<<"\n";
+            cout<<right-left+1-max<int>((prefix[right]-prefix[left]-1),0)<<"\n";
         }
-        // int ans=prefix[right]-prefix[left];
-        // cout<<ans<<"\n";
         else
         {
-            // int ans=prefix[right]-prefix[left-1];
-            // if(left>=3)
-            // {
-            //     if(arr[left-2]>=arr[left-1] and arr[left-1]>=arr[left])
-            //     {
-            //         // ans-=1;
-            //         ans+=1;
-            //     }
-            // }
-            // if(left>=2)
-            // {
-            //     if(arr[left-1]>=arr[left] and arr[left]>=arr[left+1])
-            //     {
-            //         // ans-=1;
-            //         ans+=1;
-            //     }
-            // }
-            // cout<<ans<<"\n";
-            // int maximum=max<int>(prefix[right]-prefix[left],0);
-            // cout<<right-left+1-maximum<<"\n";
-            cout<<right-left+1-max((prefix[right]-prefix[left]),0)<<"\n";
+            cout<<right-left+1-max<<int>((prefix[right]-prefix[left]),0)<<"\n";
         }
-        // cout<<ans<<"\n";
     }
 }
+
 int main()
 {
-    int testCases;
-    // cin>>testCases;
-    while(testCases--)
-    {
-        sol();
-    }
+    sol();
 }
