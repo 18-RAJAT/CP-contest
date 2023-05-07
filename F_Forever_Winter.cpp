1c62ea55187bc4e59ec65459a20e23caa22a0fa8
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-05-07 12:26:01 +0530

    07/05/23

---
+++ F_Forever_Winter.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+
+void probF()
+{
+    int n,m;
+    std::cin>>n>>m;
+
+    std::vector<std::vector<int>>adj(n+1);
+    for(int i=0;i<=m-1;++i)
+    {
+        int u,v;
+        std::cin>>u>>v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    std::unordered_set<int>nbg;
+    for(int i=0;i<=n;++i)
+    {
+        // nbg.insert(i);
+        if(adj[i].size()==1)
+        {
+            // nbg.insert(i);
+            nbg.insert(adj[i].front());
+        }
+    }
+    int y=0;
+    for(auto& it:adj[*nbg.begin()])
+    {
+        // cout<<it<<" ";
+        y+=(adj[it].size()==1);
+    }
+    std::cout<<nbg.size()<<" "<<y<<"\n";
+}
+
+int main()
+{
+    int tc;
+    std::cin>>tc;
+    while(tc--)
+    {
+        probF();
+    }
+    return 0;
+}
