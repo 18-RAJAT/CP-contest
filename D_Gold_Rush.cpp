0fe4f116171de915322f874f50d599d8d58e564b
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-05-07 05:39:28 +0530

    06/05/23

--- D_Gold_Rush.cpp
+++ D_Gold_Rush.cpp
@@ -3,7 +3,36 @@ using namespace std;
 
 void sol()
 {
-
+    int x,y;
+    cin>>x>>y; 
+    if(x==y)
+    { 
+        cout<<"YES"<<"\n"; 
+        return; 
+    } 
+    int cnt=0; 
+    int cnt1=0; 
+    int G=__gcd(x,y); 
+    x/=G;
+    y/=G; 
+    while(x%3==0)
+    { 
+        cnt++;
+        x/=3; 
+    } 
+    while(y%2==0)
+    { 
+       cnt1++;
+       y/=2; 
+    } 
+    if(x==1 and y ==1 and cnt1<=cnt) 
+    { 
+        cout<<"YES"<<"\n"; 
+    } 
+    else
+    { 
+        cout<<"NO"<<"\n"; 
+    }
 }
 int main()
 {
