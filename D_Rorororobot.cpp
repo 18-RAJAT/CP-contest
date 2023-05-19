012ab64aa75c0ef0263d0eb7828657d45db01706
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-05-19 16:07:33 +0530

    19/05/23

---
+++ D_Rorororobot.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sol()
+{
+    int n,m;
+    cin>>n>>m;
+    int arr[n];
+    memset(arr,0,sizeof(arr));
+}
+
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc-->0)
+    {
+        sol();
+    }
+    return 0;
+}
