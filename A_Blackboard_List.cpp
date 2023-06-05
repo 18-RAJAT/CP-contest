498de252549b68577f4fdaf806e4a45ccaec825f
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-06-05 07:52:48 +0530

    05/06/23

--- A_Blackboard_List.cpp
+++ A_Blackboard_List.cpp
@@ -10,21 +10,28 @@ void sol()
     for(auto&it:arr)
     {
         cin>>it;
-        if(it<0)
-        {
-            count++;
-        }
+        // if(it<0)
+        // {
+        //     count++;
+        // }
     }
-    sort(arr.begin(),arr.end());
-    if(count==0)
-    {
-        cout<<arr[n-1]-arr[0]<<"\n";
-    }
-    else
-    {
+    // sort(arr.begin(),arr.end());
+    // if(count==0)
+    // {
+    //     cout<<arr[n-1]-arr[0]<<"\n";
+    // }
+    // else
+    // {
 
-        cout<<arr[0]<<"\n";
+    //     cout<<arr[0]<<"\n";
+    // }
+    bool negative=false;
+    for(auto&it:arr)
+    {
+        negative|=(it<0);
     }
+    cout<<(negative?*min_element(arr.begin(),arr.end()):*max_element(arr.begin(),arr.end()))<<"\n";
+    // accumulate(arr.begin(),arr.end(),0))<<"\n";
 }
 int main()
 {
