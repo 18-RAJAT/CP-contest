22ce8d863200eb0cad21cd2eed9d292702249a27
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-07-26 09:30:00 +0530

    26/07/23

--- D_Prefix_Permutation_Sums.cpp
+++ D_Prefix_Permutation_Sums.cpp
@@ -1,16 +1,56 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-void sol()
-{
-    
-}
 int main()
 {
 	int t;
 	cin>>t;
 	while(t--)
 	{
-		sol();
-    }
+		ll n;
+		cin>>n;
+		vector<ll>a(n);
+		a.resize(n-1);
+		for(ll i=0;i<n-1;++i)
+		{
+			cin>>a[i];
+		}
+		vector<ll>b={a[0]};
+		for(ll i=1;i<n-1;++i)
+		{
+			b.push_back(a[i]-a[i-1]);
+		}
+		multiset<ll>aa,bb;
+		for(auto u:b)
+		{
+			bb.insert(u);
+		}
+		for(ll i=1;i<=n;++i)
+		{
+			if(bb.find(i)!=bb.end())
+			{
+				bb.erase(bb.find(i));
+				aa.insert(i);
+			}
+		}
+		if(bb.empty())
+		{
+			cout<<"YES\n";
+			continue;
+		}
+		if(bb.size()!=1)
+		{
+			cout<<"NO\n";
+			continue;
+		}
+		ll sum=0;
+		for(ll i=1;i<=n;++i)
+		{
+			if(aa.find(i)==aa.end())
+			{
+				sum+=i;
+			}
+		}
+		cout<<(sum!=*bb.begin()?"NO\n":"YES\n");
+	}
 }
