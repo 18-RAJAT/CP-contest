a9559ac6b102c9b5e803f96305e84b3517a63b22
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-09-23 09:10:39 +0530

    23/09/23

--- D_Walking_Between_Houses.cpp
+++ D_Walking_Between_Houses.cpp
@@ -5,76 +5,30 @@ void sol()
 {
     int n,k,s;
     cin>>n>>k>>s;
-    vector<int>ans;
-    if(s<n || (n-1)*k<s)
+    int pos=1,mn=0,mx=0;
+    mn=k,mx=(n-1)*k;
+    if(s<mn || s>mx)
     {
         cout<<"NO"<<endl;
         return;
     }
-    int v=n,p=1,prev=1;
-    int r,d;
-    for(int i=0;i<k;i++)
+    cout<<"YES"<<endl;
+    for(int i=k-1;i>=0;--i)
     {
-        r=k-i;
-        if(s%r==0)
+        int leave=i;
+        int reach=min(n-1,s-leave);
+        s-=reach;
+        if(pos+reach<=n)
         {
-            d=s/r;
-            for(int j=0;j<r;j++)
-            {
-                if(prev>d)
-                {
-                    prev-=d;
-                }
-                else
-                {
-                    prev+=d;
-                }
-                ans.emplace_back(prev);
-            }
-            break;
-        }
-        else if(s-abs(v-p)<r)
-        {
-            bool f=false;
-            if(v<p)
-            {
-                f=true;
-            }
-            while(s-abs(v-p)<r)
-            {
-                if(f)
-                {
-                    v++;
-                }
-                else
-                {
-                    p++;
-                }
-            }
-            if(f)
-            {
-                ans.emplace_back(v);
-            }
-            else
-            {
-                ans.emplace_back(p);
-            }
-            swap(v,p);
+            pos+=reach;
+            cout<<pos<<" ";
         }
         else
         {
-            ans.emplace_back(v);
-            prev=v;
-            s-=abs(v-p);
-            swap(v,p);
+            pos-=reach;
+            cout<<pos<<" ";
         }
     }
-    cout<<"YES"<<endl;
-    for(auto& it:ans)
-    {
-        cout<<it<<" ";
-    }
-    cout<<endl;
 }
 signed main()
 {
