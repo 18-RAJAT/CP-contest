7c55a5934e0f322427510e7ff5a848d9d303dcda
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-09-24 10:58:28 +0530

    24/09/23

---
+++ A_Bad_Triangle.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+    }
+    if(a[0]+a[1]<=a[n-1])
+    {
+        cout<<1<<" "<<2<<" "<<n<<endl;
+    }
+    else
+    {
+        cout<<-1<<endl;
+    }
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
