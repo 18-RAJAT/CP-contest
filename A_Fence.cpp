28eb17ba958c9227fc6cf9648643691a2874483f
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-10-01 07:58:12 +0530

    01/10/23

---
+++ A_Fence.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<max({a,b,c})<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
