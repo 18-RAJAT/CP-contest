d9a0c47bbf822be4dd3e733cbca1bf4451f5ff6e
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-10-15 10:16:47 +0530

    15/10/23

---
+++ C_Stack_of_Presents.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n,m;
+    cin>>n>>m;
+    vector<int>a(n);
+    for(auto& it:a)
+    {
+        cin>>it;
+    }
+    int ctr1,ctr2;
+    ctr1=ctr2=0;
+    vector<int>pos(n);
+    for(auto& it:pos)
+    {
+        cin>>it;
+        // --it;    
+    }
+    multiset<int>st;
+    int ans=0;
+    while(ctr2<m)
+    {
+        if(st.find(pos[ctr2])!=st.end())
+        {
+            st.erase(st.find(pos[ctr2]));
+            // ans+=a[pos[ctr2]];
+            ans++;
+        }
+        else
+        {
+            int tmp=pos[ctr2];
+            ans+=(2*(st.size()));
+            // int vals=0;
+            while(tmp!=a[ctr1])
+            {
+                // if(st.find(tmp)==st.end())
+                // {
+                //     st.insert(tmp);
+                //     vals+=a[tmp];
+                // }
+                st.insert(a[ctr1]);
+                ctr1++;
+                //vals++;
+                ans+=2LL;
+                // tmp=pos[++ctr1];
+            }
+            ctr1++;
+            //vals++;
+            // int get=2*vals;
+            ans++;
+        }
+        ctr2++;
+    }
+    cout<<ans<<"\n";
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
