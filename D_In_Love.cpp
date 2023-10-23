a99488deb68a337acc9a1ae41c143061c5362295
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-10-23 08:03:02 +0530

    23/10/23

--- D_In_Love.cpp
+++ D_In_Love.cpp
@@ -3,15 +3,41 @@ using namespace std;
 #define int long long
 void sol()
 {
-    
+    int q;
+    cin>>q;
+    multiset<pair<int,int>>mp;
+    multiset<int>m;
+    char c;
+    int a,b;
+    while(q--)
+    {
+        cin>>c>>a>>b;
+        if(c=='+')
+        {
+            mp.insert(make_pair(a,b));
+            m.insert(b);
+        }
+        else
+        {
+            mp.erase(mp.find(make_pair(a,b)));
+            m.erase(m.find(b));
+        }
+        if(mp.empty())
+        {
+            cout<<"NO\n";
+        }
+        else if(*m.begin()<mp.rbegin()->first)
+        {
+            cout<<"YES\n";
+        }
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
 }
 signed main()
 {
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        sol();
-    }
+    sol();
     return 0;
 }
