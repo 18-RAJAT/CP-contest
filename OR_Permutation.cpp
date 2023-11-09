63c4503bca95a27df082cdb1eb134caea66f7fab
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-11-09 10:33:09 +0530

    09/11/23

---
+++ OR_Permutation.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    for(int i=n;i>0;--i)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
