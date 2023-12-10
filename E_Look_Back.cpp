200393c70d2bc3002df59c996373ded3e8a2ad03
18-RAJAT <Rajatjoshi7535@gmail.com>
2023-12-10 20:22:21 +0530

    10/12/23

--- E_Look_Back.cpp
+++ E_Look_Back.cpp
@@ -5,37 +5,42 @@ void sol()
 {
     int n;
     cin>>n;
-    vector<int> a(n);
+    int a[n];
     for(int i=0;i<n;++i)
     {
         cin>>a[i];
     }
-    int count=0;
-    while(true)
+    int ans=0;
+    vector<int>check(n,0);
+    for(int i=n-1;i>=1;--i)
     {
-        bool flag=true;
-        for(int i=1;i<n;++i)
+        int tmp=a[i];
+        while(tmp<a[i-1])
         {
-            if(a[i-1]>a[i])
-            {
-                flag=false;
-                break;
-            }
+            tmp*=2;
+            ++check[i];
         }
-        if(flag)
+        tmp=a[i-1];
+        while(tmp<=a[i])
         {
-            break;
+            tmp*=2;
+            --check[i];
         }
-        for(int i=1;i<n;++i)
+        if(check[i]<0)
         {
-            if(a[i-1]>a[i])
-            {
-                a[i]*=2;
-                ++count;
-            }
+            check[i]++;
         }
     }
-    cout<<count<<"\n";
+    for(int i=1;i<n;++i)
+    {
+        check[i]=max<int>(0LL,check[i-1]+check[i]);
+    }
+    int sum=0;
+    for(int i=0;i<n;++i)
+    {
+        sum+=check[i];
+    }
+    cout<<sum<<endl;
 }
 signed main()
 {
