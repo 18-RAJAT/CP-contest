df61626077bd824ebd6177e93d958bd64b85d609
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-01-07 07:41:06 +0530

    07/01/24

--- D_01_Tree.cpp
+++ D_01_Tree.cpp
@@ -6,44 +6,58 @@ void sol()
     int n;
     cin>>n;
     vector<int> a(n);
+    int hit=0;
     for(int i=0;i<n;++i)
     {
         cin>>a[i];
+        if(a[i]==0)
+        {
+            hit++;
+        }
     }
-    if(a[0]==1)
+    if(hit!=1)
     {
         cout<<"NO"<<endl;
         return;
     }
-    bool visited=false;
-    queue<int>q;
-    q.push(0);
-    int i=0;
-    while(!q.empty())
+    stack<int>st1,st2;
+    st1.push(0);
+    st2.push(n-1);
+    vector<int>right(n),left(n);
+    right.assign(n,0);
+    left.assign(n,0);
+    for(int i=0;i<n;++i)
     {
-        int x=q.front();
-        q.pop();
-        if(x>=n)
+        while(!st1.empty() && a[i]<a[st1.top()])
         {
-            break;
+            right[st1.top()]=a[i];
+            // cout<<st1.top()<<" "<<a[i]<<endl;
+            st1.pop();
         }
-        if(x==n-1)
+        st1.push(i);
+    }
+    for(int i=n-1;~i;--i)
+    {
+        while(!st2.empty() && a[i]<a[st2.top()])
         {
-            visited=true;
-            break;
+            left[st2.top()]=a[i];
+            // cout<<st2.top()<<" "<<a[i]<<endl;
+            st2.pop();
         }
-        q.push(2*x+1);
-        q.push(2*x+2);
-        i++;
+        st2.push(i);
     }
-    if(visited)
+    for(int i=0;i<n;++i)
     {
-        cout<<"YES"<<endl;
+        if(a[i]!=0)
+        {
+            if(left[i]!=a[i]-1 && right[i]!=a[i]-1)
+            {
+                cout<<"NO"<<endl;
+                return;
+            }
+        }
     }
-    else
-    {
-        cout<<"NO"<<endl;
-    }
+    cout<<"YES"<<endl;
 }
 signed main()
 {
