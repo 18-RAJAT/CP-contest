0316215b20c77168a18b57a955bc044262ade69f
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-01-19 06:07:06 +0530

    19/01/24

--- E_Increasing_Subsequences.cpp
+++ E_Increasing_Subsequences.cpp
@@ -1,55 +1,35 @@
-// #include <iostream>
-// #include <vector>
-
-// using namespace std;
-
-// int main() {
-//     int t;
-//     cin >> t;
-
-//     for (int i = 0; i < t; ++i) {
-//         vector<int> output_answer;
-//         int intended_number;
-//         cin >> intended_number;
-//         intended_number -= 1;
-
-//         int increasing_subsequence_number = 1;
-//         int index = 0;
-
-//         while (intended_number >= increasing_subsequence_number) {
-//             intended_number -= increasing_subsequence_number;
-//             increasing_subsequence_number *= 2;
-//             output_answer.push_back(index);
-//             index += 1;
-//         }
-
-//         index = 10000;
-
-//         while (intended_number != 0) {
-//             if (intended_number >= (1 << index)) {
-//                 output_answer.push_back(index);
-//                 intended_number -= 1 << index;
-//             }
-//             index -= 1;
-//         }
-
-//         cout << output_answer.size() << endl;
-//         for (int j = 0; j < output_answer.size(); ++j) {
-//             cout << output_answer[j] << " ";
-//         }
-//         cout << endl;
-//     }
-
-//     return 0;
-// }
-
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
 void sol()
 {
-    int n;
-    
+    int x;
+    cin>>x;
+    int seq=-1;
+    int temp=x;
+    while(temp)
+    {
+        temp>>=1;
+        seq++;
+    }
+    vector<int>ans;
+    for(int i=0;i<seq;++i)
+    {
+        ans.push_back(2*i+1);
+    }
+    for(int i=seq-1;~i;--i)
+    {
+        if(((long long)1<<i)&x)
+        {
+            ans.push_back(2*i);
+        }
+    }
+    cout<<ans.size()<<endl;
+    for(auto& it:ans)
+    {
+        cout<<it<<" ";
+    }
+    cout<<endl;
 }
 signed main()
 {
