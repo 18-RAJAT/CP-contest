db3a9694d52eb4ab982fff8afca8cacc0f1bba82
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-02-16 09:50:21 +0530

    16/02/24

--- D_Sasha_and_a_Walk_in_the_City.cpp
+++ D_Sasha_and_a_Walk_in_the_City.cpp
@@ -1,54 +1,48 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-const int MOD = 998244353;
-
-vector<vector<int>> adj;
-vector<int> dp_in, dp_out;
-
-void dfs(int u, int parent) {
-    dp_in[u] = 1;
-    for (int v : adj[u]) {
-        if (v != parent) {
-            dfs(v, u);
-            dp_in[u] = (1LL * dp_in[u] * (dp_out[v] + dp_in[v])) % MOD;
+#define int long long
+int ans=0;
+void sol()
+{
+    int n;
+    cin>>n;
+    vector<vector<int>>adj;
+    function<int(int,int)>dfs=[&](int u,int p)->int
+    {
+        int sum=1;
+        for(auto& v:adj[u])
+        {
+            if(v==p)
+            {
+                continue;
+            }
+            int call=dfs(v,u);
+            ans=(ans+call+998244352)%998244353;
+            sum=(sum*call)%998244353;
         }
+        sum=(sum+1)%998244353;
+        return sum%998244353;
+    };
+    ans=0;
+    adj.assign(n,vector<int>());
+    for(int i=0;i<n-1;++i)
+    {
+        int u,v;
+        cin>>u>>v;
+        --u,--v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
-    dp_out[u] = 1;
-    for (int v : adj[u]) {
-        if (v != parent) {
-            dp_out[u] = (1LL * dp_out[u] * (dp_out[v] + dp_in[v])) % MOD;
-        }
-    }
+    int tmp=dfs(0,-1)%998244353;
+    cout<<(ans+tmp)%998244353<<endl;
 }
-
-int main() {
+signed main()
+{
     int t;
-    cin >> t;
-    
-    while (t--) {
-        int n;
-        cin >> n;
-        
-        adj.assign(n + 1, vector<int>());
-        dp_in.assign(n + 1, 0);
-        dp_out.assign(n + 1, 0);
-        
-        for (int i = 0; i < n - 1; ++i) {
-            int u, v;
-            cin >> u >> v;
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        
-        dfs(1, 0);
-        
-        int ans = (dp_in[1] + dp_out[1]) % MOD;
-        cout << ans << endl;
+    cin>>t;
+    while(t--)
+    {
+        sol();
     }
-    
     return 0;
 }
