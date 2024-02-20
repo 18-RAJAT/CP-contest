d79cff100dc0e779799a2aef1c7654ef35398271
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-02-20 10:25:17 +0530

    20/02/24

--- C_Vlad_and_a_Sum_of_Sum_of_Digits.cpp
+++ C_Vlad_and_a_Sum_of_Sum_of_Digits.cpp
@@ -1,35 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+int arr[2000005];
+void func()
+{
+    for(int i=1;i<=2000000;++i)
+    {
+        int x=i;
+        if(x>=10)
+        {
+            int sum=0;
+            while(x)
+            {
+                sum+=x%10;
+                x/=10;
+            }
+            x=sum;
+        }
+        arr[i]=arr[i-1]+x;
+    }
+}
 void sol()
 {
     int n;
     cin>>n;
-    vector<int>arr;
-    for(int i=1;i<=n;++i)
-    {
-        arr.push_back(i);
-    }
-    vector<int>ans;
-    for(int i=0;i<n;++i)
-    {
-        int sum=0;
-        while(arr[i]>0)
-        {
-            sum+=arr[i]%10;
-            arr[i]/=10;
-        }
-        ans.push_back(sum);
-    }
-    int result=0;
-    for(int i=0;i<ans.size();++i)
-    {
-        result+=ans[i];
-    }
-    cout<<result<<endl;
+    cout<<arr[n]<<endl;
 }
 signed main()
 {
+    func();
     int t;
     cin>>t;
     while(t--)
