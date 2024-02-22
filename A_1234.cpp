8bdf155082ebff3261d60cfd8d14b480294eeabc
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-02-22 20:04:00 +0530

    21/02/24

---
+++ A_1234.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    if(n==1)cout<<1;
+    else if(n==2)cout<<4;
+    else if(n==3)cout<<89;
+    else cout<<695;
+}
+signed main()
+{
+    sol();
+    return 0;
+}
