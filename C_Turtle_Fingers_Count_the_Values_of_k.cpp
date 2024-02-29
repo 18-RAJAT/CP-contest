a78ca8036f73e66b4f2ad8517249d84ef9ff916e
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-02-29 10:07:47 +0530

    29/02/24

---
+++ C_Turtle_Fingers_Count_the_Values_of_k.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int a,b,l;
+    cin>>a>>b>>l;
+    set<int>s;
+    int ans=1;
+    while(l%ans==0)
+    {
+        int y=1;
+        while((l/ans)%y==0)
+        {
+            s.insert((l/ans)/y);
+            y*=b;
+        }
+        ans*=a;
+    }
+    cout<<s.size()<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
