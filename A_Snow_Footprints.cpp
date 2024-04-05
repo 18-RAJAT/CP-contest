e862247d56a31c6946662d46c21b829f550c31f1
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-04-06 00:58:30 +0530

    06/04/24

--- A_Snow_Footprints.cpp
+++ A_Snow_Footprints.cpp
@@ -8,7 +8,6 @@ void sol()
     string s;
     cin>>s;
     cout<<(s.find('R')!=string::npos?s.find_first_of('R')+1:s.find_last_of('L')+1)<<" "<<(s.find('R')!=string::npos?s.find_last_of('R')+2:s.find_first_of('L'));
-
 }
 signed main()
 {
