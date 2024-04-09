02834a06e09a2ee95df21442429af2f4b3973583
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-04-09 08:48:34 +0530

    09/04/24

--- C_Inhabitant_of_the_Deep_Sea.cpp
+++ C_Inhabitant_of_the_Deep_Sea.cpp
@@ -1,70 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-int ships(int n,int k,vector<int>&durability)
-{
-    int tot=0;
-    for(int i=0;i<n;++i)
-    {
-        tot+=durability[i];
-    }
-    if(k>=tot)
-    {
-        return n;
-    }
-    int ans=0;
-    int f=durability[0];
-    int l=durability[n-1];
-    while(k>0 && n>0)
-    {
-        if(f<=l)
-        {
-            if(f==0)
-            {
-                ans++;
-                n--;
-                if(n>0)
-                {
-                    f=durability[0];
-                }
-            }
-            else
-            {
-                f--;
-            }
-            k--;
-        }
-        else
-        {
-            if(l==0)
-            {
-                ans++;
-                n--;
-                durability.pop_back();
-                if(n>0)
-                {
-                    l=durability[n-1];
-                }
-            }
-            else
-            {
-                l--;
-            }
-            k--;
-        }
-    }
-    return ans;
-}
 void sol()
 {
     int n,k;
     cin>>n>>k;
-    vector<int>d(n);
+    vector<int>a(n);
     for(int i=0;i<n;++i)
     {
-        cin>>d[i];
+        cin>>a[i];
     }
-    cout<<ships(n,k,d)<<endl;
+    int front=(k+1)/2;
+    int back=k-front;
+    for(int i=0;i<n;++i)
+    {
+        (front>=a[i])?(front-=a[i],a[i]=0):(a[i]-=front,front=0);
+    }
+    for(int i=n-1;~i;--i)
+    {
+        (back>=a[i])?(back-=a[i],a[i]=0):(a[i]-=back,back=0);
+    }
+    int res=0;
+    for(int i=0;i<n;++i)
+    {
+        if(a[i]<=0)res++;
+    }
+    cout<<res<<endl;
 }
 signed main()
 {
