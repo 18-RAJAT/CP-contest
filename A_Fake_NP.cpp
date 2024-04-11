bd407c45927df873a874c0d0f7140593b05c2bbc
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-04-11 08:33:14 +0530

    11/04/24

---
+++ A_Fake_NP.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int l,r;
+    cin>>l>>r;
+    cout<<(l==r?l:2);
+}
+signed main()
+{
+    sol();
+    return 0;
+}
