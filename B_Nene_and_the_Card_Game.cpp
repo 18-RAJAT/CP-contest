f68e5a0ef081b5873a742c52f70bcbf13d01811a
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-04-15 09:49:45 +0530

    15/04/24

---
+++ B_Nene_and_the_Card_Game.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+    }
+    int ans=0;
+    sort(a,a+n);
+    for(int i=0;i<n;++i)
+    {
+        if(a[i]==a[i+1])
+        {
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
