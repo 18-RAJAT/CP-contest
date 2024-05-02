0993fbe3ddfb58bfa8991782d1406bd82bba1240
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-05-02 22:30:42 +0530

    contest CF 02/05/24

---
+++ A_Maximize.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int x;
+    cin>>x;
+    cout<<x-1<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
