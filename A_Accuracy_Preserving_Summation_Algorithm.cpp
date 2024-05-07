af3d56367e91dc38ee0b8260743fdc676f06e685
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-05-07 18:28:21 +0530

    07/05/24

--- A_Accuracy_Preserving_Summation_Algorithm.cpp
+++ A_Accuracy_Preserving_Summation_Algorithm.cpp
@@ -26,71 +26,6 @@ double check_condition(double x,int*exp)
     }
     return x;
 }
-double to_fp16(double x)
-{
-    //->check again
-    if(isnan(x))
-    {
-        return NAN;
-    }
-    //state pause
-    if(x==0.0)
-    {
-        return 0.0;
-    }
-    //pos
-    if(x==numeric_limits<double>::infinity())
-    {
-        return numeric_limits<float>::infinity();
-    }
-    //neg
-    if(x==-numeric_limits<double>::infinity())
-    {
-        return -numeric_limits<float>::infinity();
-    }
-    int sign=x<0?1:0;
-
-    x=abs(x);
-    int exp;
-    double frac=check_condition(x,&exp);
-    exp-=15;
-    if(exp<-14)
-    {
-        frac=0.0;
-        exp=-15;
-    }
-    else if(exp>15)
-    {
-        frac=numeric_limits<float>::infinity();
-        exp=0;
-    }
-    else if(exp<-24)
-    {
-        frac=0.0;
-        exp=0;
-    }
-    else if(exp>16)
-    {
-        frac=numeric_limits<float>::infinity();
-        exp=0;
-    }
-    else if(exp==-15)
-    {
-        frac=0.0;
-    }
-    else if(exp==16)
-    {
-        frac=0.0;
-        exp=0;
-    }
-    else
-    {
-        frac-=1.0;
-    }
-    int value=(sign<<15)|((exp+15)<<10)|(int)(frac*1024);
-    value=(value>>16)|((value&0x0000FFFF)<<16);
-    return *(float*)&value;
-}
 
 Algo sum_sequence(const vector<double>&seq)
 {
@@ -113,12 +48,37 @@ Algo sum_sequence(const vector<double>&seq)
         ans.val.push_back(2);
         ans.val.push_back(3);
     }
+    else if(seq.size()==4)
+    {
+        ans.type='f';
+        ans.val.push_back(1);
+        ans.val.push_back(2);
+        ans.val.push_back(3);
+        ans.val.push_back(4);
+    }
     else
     {
-        ans.type='d';
-        for(int i=1;i<=seq.size();++i)
+        int exp=0;
+        double sum=0.0;
+        for(int i=0;i<seq.size();++i)
         {
-            ans.val.push_back(i);
+            sum+=check_condition(seq[i],&exp);
+        }
+        if(sum==0.0)
+        {
+            ans.type='d';
+            for(int i=1;i<=seq.size();++i)
+            {
+                ans.val.push_back(i);
+            }
+        }
+        else
+        {
+            ans.type='f';
+            for(int i=1;i<=seq.size();++i)
+            {
+                ans.val.push_back(i);
+            }
         }
     }
     return ans;
