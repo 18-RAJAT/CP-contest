7f2f2a76f9c107703b6e50fb40a736f7b2820861
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-05-29 12:51:21 +0530

    29/05/24

--- C_1_Game_on_Tree_Easy.cpp
+++ C_1_Game_on_Tree_Easy.cpp
@@ -1,43 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-vector<int>adj[200005];
-int n,t;
-bool dfs(int node,int parent,int target)
-{
-    if(node==target)
-    {
-        return true;
-    }
-    for(auto it:adj[node])
-    {
-        if(it!=parent && !dfs(it,node,target))
-        {
-            return false;
-        }
-    }
-    return false;
-}
 void sol()
 {
+    int n,t,u,v;
     cin>>n>>t;
+    int count=0;
+    vector<int>a(n+1);
     for(int i=0;i<n-1;++i)
     {
-        int u,v;
         cin>>u>>v;
-        adj[u-1].push_back(v-1);
-        adj[v-1].push_back(u-1);
+        a[v]=u;
     }
-    int vis;
-    cin>>vis;
-    if(dfs(vis-1,-1,0) && dfs(vis-1,-1,n-1) && t%3==0)
+    int x;
+    cin>>x;
+    while(a[x]!=0)
     {
-        cout<<"Hermione"<<endl;
+        x=a[x],count++;
     }
-    else
-    {
-        cout<<"Ron"<<endl;
-    }
+    cout<<(((count&1) || (n-count-1)&1)?"Ron":"Hermione");
 }
 signed main()
 {
