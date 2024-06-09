9935cea87af56b17cb6fb38034c6455ca6c8bb04
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-06-10 00:22:50 +0530

    10/06/24

--- B_Large_Addition.cpp
+++ B_Large_Addition.cpp
@@ -1,36 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-bool recur(const string& s)
-{
-    string f="01234";
-    string l="56789";
-    return s.find(f)!=string::npos && s.find(l)!=string::npos;
-}
 void sol()
 {
-    int n;
-    cin>>n;
-    string s=to_string(n);
-    if (recur(s))
-    {
-        cout<<"NO"<<endl;
-        return;
-    }
-    int ok=0,sum=0,tot=5+6+7+8+9;
-    for(int i =0;i<s.size()-1;++i)
-    {
-        sum+=(s[i]-'0');
-        if(s[i]=='0')ok=1;
-    }
-    if(s.back()=='9'||sum==tot||s[0]!='1'||s.size()%10==0 || ok)
-    {
-        cout<<"NO"<<endl;
-    }
-    else
-    {
-        cout<<"YES"<<endl;
-    }
+    string s;
+    cin>>s;
+    int ok=s[0]=='1' && s.back()<'9';
+    for(int i=1;i+1<s.size();++i)ok&=s[i]>'0';
+    cout<<(ok?"YES":"NO")<<endl;
 }
 signed main()
 {
