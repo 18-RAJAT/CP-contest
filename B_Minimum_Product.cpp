ba66d26490c0ec6e927fbfeb47221f4593b1efdb
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-06-23 09:37:30 +0530

    23/06/24

--- B_Minimum_Product.cpp
+++ B_Minimum_Product.cpp
@@ -3,7 +3,18 @@ using namespace std;
 #define int long long
 void sol()
 {
-    
+    int a,b,x,y,n;
+    cin>>a>>b>>x>>y>>n;
+    int d1=a-x,d2=b-y,ans,ans1,ans2;
+    if(d1+d2<=n)
+    {
+        cout<<x*y<<endl;
+        return;
+    }
+    ans1=(a-min(d1,n))*(b-min(n-min(d1,n),d2));
+    ans2=(b-min(d2,n))*(a-min(n-min(d2,n),d1));
+    ans=min(ans1,ans2);
+    cout<<ans<<endl;
 }
 signed main()
 {
