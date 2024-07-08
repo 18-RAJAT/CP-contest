687eac45a35f4ac7d3278a7547341fbc91bb3ac2
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-07-08 09:54:37 +0530

    08/07/24

---
+++ A_Array_Divisibility.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;++i)
+    {
+        cout<<i<<" \n"[i==n];
+    }
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
