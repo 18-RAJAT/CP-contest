d78ebef204883585810300f0a6d40aaeb851c416
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-07-23 12:22:53 +0530

    23/07/24

--- C_Quiz_Master.cpp
+++ C_Quiz_Master.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long
 void sol()
 {
     int n,m;
@@ -10,24 +9,20 @@ void sol()
     {
         cin>>a[i];
     }
-    int ans=2e9;
+    int ans=1e9;
     multiset<int>ms;
     sort(a.begin(),a.end());
     for(int i=0;i<n;++i)
     {
         for(int j=1;j*j<=a[i];++j)
+        if(a[i]%j==0)
         {
-            if(a[i]%j==0)
+            for(auto& k:{j,a[i]/j})
             {
-                vector<int>vals={j,a[i]/j};
-                for(int idx=0;idx<vals.size();++idx)
-                {
-                    int k=vals[idx];
-                    if(m<k)continue;
-                    if(~check[k])ms.erase(ms.find(check[k]));
-                    check[k]=i;
-                    ms.insert(check[k]);
-                }
+                if(m<k)continue;
+                if(~check[k])ms.erase(ms.find(check[k]));
+                check[k]=i;
+                ms.insert(check[k]);
             }
         }
         if(ms.size()==m)
@@ -35,7 +30,7 @@ void sol()
             ans=min(ans,a[*ms.rbegin()]-a[*ms.begin()]);
         }
     }
-    if(ans==2e9)ans=-1;
+    if(ans==1e9)ans=-1;
     cout<<ans<<endl;
 }
 signed main()
