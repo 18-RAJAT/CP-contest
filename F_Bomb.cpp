4ddfe1d520f3734bf83ab4f41fba2f7eb8dd2851
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-07-27 09:52:35 +0530

    27/07/24

--- F_Bomb.cpp
+++ F_Bomb.cpp
@@ -8,40 +8,29 @@ void sol()
     vector<int>a(n),b(n);
     for(int i=0;i<n;++i)cin>>a[i];
     for(int i=0;i<n;++i)cin>>b[i];
-    vector<pair<int,int>>heap(n);
-    for(int i=0;i<n;++i)heap[i]=make_pair(a[i],i);
-    make_heap(heap.begin(),heap.end(),greater<pair<int,int>>());
+    int l=0,r=1e9;
     int ans=0;
-    for(int i=0;i<k;++i)
+    while(l<r)
     {
-        pop_heap(heap.begin(),heap.end(),greater<pair<int,int>>());
-        auto [p,q]=heap.back();
-        heap.pop_back();
-        // int tmp=0;
-        // for(int j=1;j<n;++j)
-        // {
-        //     if(a[tmp]<a[j])tmp=j;
-        // }
-        // int maxi=max_element(a.begin(),a.end())-a.begin();
-        // ans+=a[maxi];
-        if(p==0)break;
-        // a[maxi]=max<int>(0,a[maxi]-b[maxi]);
-        ans+=p;
-        // if(all_of(a.begin(),a.end(),[](int check){return check==0;}))
-        int maxi=max<int>(0,p-b[q]);
-        if(maxi>=1)
+        int m=(l+r)>>1;
+        int check=0;
+        for(int i=0;i<n;++i)
         {
-            heap.push_back(make_pair(maxi,q));
-            push_heap(heap.begin(),heap.end(),greater<pair<int,int>>());
+            int temp=a[i]-m;
+            if(temp<=0)continue;
+            check+=(temp+b[i]-1)/b[i];
         }
-        // {
-        //     break;
-        // }
-        // if(a[tmp]==0)break;
-        // ans+=a[tmp];
-        // a[tmp]=max<int>(0,a[tmp]-b[tmp]);
+        (check<=k)?r=m:l=m+1;
     }
-    cout<<ans<<endl;
+    for(int i=0;i<n;++i)
+    {
+        int temp=a[i]-l;
+        if(temp<=0)continue;
+        int checker=(temp+b[i]-1)/b[i];
+        ans+=checker*(2*a[i]-(checker-1)*b[i])/2;
+        k-=checker;
+    }
+    cout<<k*l+ans<<endl;
 }
 signed main()
 {
