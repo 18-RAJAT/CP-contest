e893811bb620b00f571a63289ec7553392bb5924
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-07-31 10:56:30 +0530

    31/07/24

--- B_Make_Three_Regions.cpp
+++ B_Make_Three_Regions.cpp
@@ -1,28 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-int recur(vector<vector<char>>& grid,int r,int c)
-{
-    if(grid[r][c]!='.')return 0;
-    int tog=r^1;
-    return c-1>=1 && grid[tog][c-1]=='x' && grid[r][c-1]=='.' && c+1<grid[0].size() && grid[tog][c+1]=='x' && grid[r][c+1]=='.';
-}
 void sol()
 {
     int n;
     cin>>n;
-    vector<vector<char>>grid(2,vector<char>(n));
+    string a,b;
+    cin>>a>>b;
     int ans=0;
-    for(auto& i:grid)
+    auto compute=[&](string& a,string& b,int n,int & ans)->void
     {
-        for(auto& j:i)cin>>j;
-    }
-    for(int i=0;i<2;++i)
-    {
-        for(int j=0;j<n;++j)
+        for(int i=1;i+1<n;++i)
         {
-            if(recur(grid,i,j))ans++;
+            if(a[i-1]=='.' && a[i]=='.' && a[i+1]=='.' && b[i-1]=='x' && b[i]=='.' && b[i+1]=='x')ans++;
         }
+    };
+    for(int j=0;j<2;++j)
+    {   compute(a,b,n,ans);
+        swap(a,b);
     }
     cout<<ans<<endl;
 }
