ffb7e7fb9333c032a534d479f4b4975e559a6eea
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-07-31 13:09:07 +0530

    31/07/24

--- D_Blocking_Elements.cpp
+++ D_Blocking_Elements.cpp
@@ -1,95 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-vector<int>a;
-class segTree
-{
-    public:
-    int n;
-    vector<int>tree;
-    segTree(int n)
-    {
-        this->n=n;
-        tree.resize(4*n);
-    }
-    void build(int node,int start,int end)
-    {
-        if(start==end)
-        {
-            tree[node]=a[start];
-        }
-        else
-        {
-            int mid=(start+end)/2;
-            build(2*node,start,mid);
-            build(2*node+1,mid+1,end);
-            tree[node]=max(tree[2*node],tree[2*node+1]);
-        }
-    }
-    void update(int node,int start,int end,int idx,int val)
-    {
-        if(start==end)
-        {
-            a[idx]=val;
-            tree[node]=val;
-        }
-        else
-        {
-            int mid=(start+end)/2;
-            if(start<=idx && idx<=mid)
-            {
-                update(2*node,start,mid,idx,val);
-            }
-            else
-            {
-                update(2*node+1,mid+1,end,idx,val);
-            }
-            tree[node]=max(tree[2*node],tree[2*node+1]);
-        }
-    }
-    int query(int node,int start,int end,int l,int r)
-    {
-        if(r<start || end<l)
-        {
-            return 0;
-        }
-        if(l<=start && end<=r)
-        {
-            return tree[node];
-        }
-        int mid=(start+end)/2;
-        int p1=query(2*node,start,mid,l,r);
-        int p2=query(2*node+1,mid+1,end,l,r);
-        return max(p1,p2);
-    }
-};
 void sol()
 {
     int n;
     cin>>n;
     vector<int>a(n);
-    for(int i=0;i<n;++i)
+    for(int i=0;i<n;++i)cin>>a[i];
+    vector<int>temp=a;
+    for(int i=1;i<n;++i)
     {
-        cin>>a[i];
+        temp[i]+=temp[i-1];
     }
-    segTree st(n);
-    st.build(1,0,n-1);
-    vector<int>dp(n);
-    for(int i=0;i<n;++i)
+    int sum=0;
+    for(int i=0;i<n;++i)sum+=a[i];
+    multiset<int>ms;
+    int l=0,r=sum,ans=r;
+    while(l<r)
     {
-        dp[i]=st.query(1,0,n-1,0,i-1);
-    }
-    vector<int>dp2(n);
-    for(int i=n-1;i>=0;--i)
-    {
-        dp2[i]=st.query(1,0,n-1,i+1,n-1);
-    }
-    int ans=0;
-    for(int i=0;i<n;++i)
-    {
-        ans=max(ans,a[i]+dp[i]+dp2[i]);
+        int last=n-1;
+        int mid=(l+r)/2;
+        vector<int>dp(n+1,0);
+        ms.clear();
+        for(int i=n-1;i>=0;--i)
+        {
+            ms.insert(dp[i+1]+a[i]);
+            int check=i?temp[i-1]:0;
+            if(temp[n-1]-check<=mid)continue;
+            dp[i]=1e18;
+
+            while(i<last && mid<temp[last-1]-check)
+            {
+            	multiset<int>::iterator it=ms.find(a[last]+dp[last+1]);
+            	if(it!=ms.end())ms.erase(it);
+            	last--;
+            }
+            dp[i]=(ms.empty())?1e18:*ms.begin();
+        }
+        if(dp[0]<=mid)ans=mid,r=mid;
+        else l=mid+1;
     }
     cout<<ans<<endl;
+    assert(n>=0);
+    assert(!a.empty());
+    assert(accumulate(a.begin(),a.end(),0LL)>=0);
 }
 signed main()
 {
