44bd7479fa091943d832bfb1d208f2277db46b5e
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-08-01 22:25:17 +0530

    01/08/24

---
+++ A_P_NP.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int p;
+    cin>>p;
+    cout<<max(p-2,0LL);
+}
+signed main()
+{
+    sol();
+    return 0;
+}
