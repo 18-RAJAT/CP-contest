c5530d80e2ec6ac3ac8ae3bc7eccc3aa902407a7
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-08-02 10:41:11 +0530

    02/08/24

---
+++ C_Phonier.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n,q;
+    cin>>n>>q;
+    vector<int>a(n),b(n+1);
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+        b[i+1]=b[i]^(a[i]+a[i]);
+    }
+    while(q--)
+    {
+        int l,r;
+        cin>>l>>r;
+        cout<<(b[l-1]^b[r])<<endl;
+    }
+}
+signed main()
+{
+    sol();
+    return 0;
+}
