8d7a3a489f58173323885ff7eb014d105ba6a9e9
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-08-06 13:15:24 +0530

    06/08/24

---
+++ A_Avoid_Trygub.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
