6708f739671c1789b8d4a040c6821b737b1aba9b
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-08-07 10:16:25 +0530

    07/08/24

--- B_Card_Game.cpp
+++ B_Card_Game.cpp
@@ -3,21 +3,14 @@ using namespace std;
 #define int long long
 void sol()
 {
-    int a1,a2,b1,b2;
-    cin>>a1>>a2>>b1>>b2;
+    int a[2],b[2];
+    cin>>a[0]>>a[1]>>b[0]>>b[1];
     int ans=0;
     for(int i=0;i<2;++i)
     {
         for(int j=0;j<2;++j)
         {
-            int x1=i==0?a1:a2;
-            int x2=i==0?a2:a1;
-            int y1=j==0?b1:b2;
-            int y2=j==0?b2:b1;
-            int ok=0;
-            if(x1>y1)ok++;
-            if(x2>y2)ok++;
-            if(ok>=2)ans++;
+            ans+=((a[i]>b[j])+(a[1-i]>b[1-j])>(a[i]<b[j])+(a[1-i]<b[1-j]));
         }
     }
     cout<<ans<<endl;
