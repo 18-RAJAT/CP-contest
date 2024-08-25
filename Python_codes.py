528a19891ca821d7dda253bb1dfd998f62153dc3
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-08-25 18:52:59 +0530

    25/08/24

--- Python_codes.py
+++ Python_codes.py
@@ -1,9 +1,31 @@
-Questions={
-    "What is the keyword to define a variable in Python?":"=",
-    "Which symbol is used for comments in Python?":"#",
-    "What function is used to print text in Python?":"print",
-    "How do you start a loop in Python?":"for",
-    "Which data type is used to store text?":"string"
-}
-your_score=sum(1 for i,j in Questions.items() if input(i+" ").lower()==j.lower())
-print(f"Your score: {your_score}/{len(Questions)}")
+Balance=0
+while 1:
+    print("\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit")
+    choice=input("Choose an option: ")
+
+    if choice=="1":
+        Amount=float(input("Enter deposit Amount: "))
+        if Amount>0:
+            Balance+=Amount
+            print(f"Deposited: ₹{Amount:.2f}")
+        else:
+            print("Deposit Amount must be positive")
+
+    elif choice=="2":
+        Amount=float(input("Enter withdrawal Amount: "))
+        if Amount<=0:
+            print("Withdrawal Amount must be positive")
+        elif Amount>Balance:
+            print("Insufficient Balance")
+        else:
+            Balance-=Amount
+            print(f"Withdrew: ₹{Amount:.2f}")
+
+
+    elif choice=="3":
+        print(f"Current Balance: ₹{Balance:.2f}")
+
+    elif choice=="4":
+        break
+    else:
+        print("Invalid")
