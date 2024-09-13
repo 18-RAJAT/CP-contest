373f34bf19aa5c40665e3adcf4ca2643a56c122c
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-09-13 09:55:59 +0530

    13/09/24

--- E_Generate_a_String.cpp
+++ E_Generate_a_String.cpp
@@ -8,14 +8,7 @@ void sol()
     int dp[n+1]={};
     for(int i=1;i<=n;++i)
     {
-        dp[i]=x+dp[i-1];
-        int precompute=dp[0]+x*abs(i)+y;
-        for(int j=1;j<i;++j)
-        {
-            int state=dp[j]+x*abs(j*2-i)+y;
-            precompute=min(precompute,state);
-        }
-        dp[i]=min(dp[i],precompute);
+        dp[i]=min(dp[i-1]+x,(i&1?x:0)+dp[(i+1)/2]+y);
     }
     cout<<dp[n];
 }
