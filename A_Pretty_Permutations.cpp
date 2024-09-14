70c04d4fa286357796d209037993aa4cc61affdc
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-09-14 12:32:09 +0530

    14/09/24

--- A_Pretty_Permutations.cpp
+++ A_Pretty_Permutations.cpp
@@ -7,18 +7,12 @@ void sol()
     cin>>n;
 	if(n%2==0)
     {
-		for(int i=2;i<=n;i+=2)
-        {
-			cout<<i<<" "<<i-1<<" ";
-		}
+		for(int i=2;i<=n;i+=2)cout<<i<<" "<<i-1<<" ";
 	}
 	else
     {
 		cout<<"3 1 2";
-		for(int j=5;j<=n;j+=2)
-        {
-			cout<<j<<" "<<j-1<<" ";
-		}
+		for(int j=5;j<=n;j+=2)cout<<j<<" "<<j-1<<" ";
 	}
 	cout<<endl;
 }
