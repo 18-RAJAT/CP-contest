3db796b1c2e84c31c43ffc145f0a2888898d4b4b
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-09-17 22:43:04 +0530

    17/09/24

---
+++ B_Three_Brothers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<(a^b);
+}
+signed main()
+{
+    sol();
+    return 0;
+}
