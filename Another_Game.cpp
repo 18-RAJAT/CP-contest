822f5410e87cc655140680a1ed3c4f6e6de9553e
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-09-28 10:32:17 +0530

    28/09/24

---
+++ Another_Game.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n;
+    cin>>n;
+    vector<int>a(n);
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+    }
+    int ans=0;
+    for(int i=0;i<n;++i)
+    {
+        if(a[i]!=i+1)ans=max(ans,a[i]+1);
+    }
+    cout<<ans<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
