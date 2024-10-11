500f47cd29ad38968221e714a83d127588d1e45d
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-10-11 09:16:58 +0530

    11/10/24

--- D_Strong_Vertices.cpp
+++ D_Strong_Vertices.cpp
@@ -1,51 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
+#define int long long
 void sol()
 {
-    ll n;
+    int n;
     cin>>n;
-    vector<ll>a(n),b(n);
-    for(ll i=0;i<n;++i)
+    vector<int>a(n),b(n);
+    for(int i=0;i<n;++i)
     {
         cin>>a[i];
     }
-    for(ll i=0;i<n;++i)
+    for(int i=0;i<n;++i)
     {
         cin>>b[i];
     }
-    vector<pair<ll,ll>>ans;
-    ll mx=INT_MIN;
-    for(ll i=0;i<n;++i)
+    vector<pair<int,int>>ans;
+    int Strong=INT_MIN;
+    for(int i=0;i<n;++i)
     {
         ans.push_back(make_pair(a[i]-b[i],i+1));
-        mx=max<int>(mx,a[i]-b[i]);
+        Strong=max<int>(Strong,a[i]-b[i]);
     }
-    vector<ll>find;
-    for(ll i=0;i<n;++i)
+    vector<int>res;
+    for(int i=0;i<n;++i)
     {
-        if(ans[i].first==mx)
+        if(ans[i].first==Strong)
         {
-            find.push_back(ans[i].second);
+            res.push_back(ans[i].second);
         }
-        // else
-        // {
-        //     if(ans[i].first>0)
-        //     {
-        //         find.push_back(ans[i].second);
-        //     }
-        // }
     }
-    cout<<find.size()<<"\n";
-    for(auto& it:find)
+    cout<<res.size()<<"\n";
+    for(int i=0;i<res.size();++i)
     {
-        cout<<it<<" ";
+        cout<<res[i]<<" ";
     }
-    cout<<"\n";
+    cout<<endl;
 }
-int main()
+signed main()
 {
-    ll t;
+    int t;
     cin>>t;
     while(t--)
     {
