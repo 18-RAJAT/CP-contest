d5db98c5bb85489b19f8a1d72fb50dfe1b8870ab
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-10-15 12:15:11 +0530

    15/10/24

--- D_Attribute_Checks.cpp
+++ D_Attribute_Checks.cpp
@@ -5,53 +5,43 @@ void sol()
 {
     int n,m;
     cin>>n>>m;
-    vector<int>a(n);
+    vector<int>a(n),b;
+    int mx=m*2+1;
+    vector<vector<int>>dp(mx);
     for(int i=0;i<n;++i)
     {
         cin>>a[i];
+        if(a[i]==0)b.push_back(i);
+        else dp[a[i]+m].push_back(i);
     }
-    vector<int>dp(m+1,0);
-    int CountZ=0;
-    for(int i=0;i<n;++i)
+    auto check=[&](int i,int j)->int
     {
-        if(a[i]==0)
+        auto& compute=dp[j+m];
+        return compute.size()-(lower_bound(compute.begin(),compute.end(),i)-compute.begin());
+    };
+    vector<vector<int>>ndp(m,vector<int>(m+1));
+    ndp[0][0]=check(b[0],-1);
+    ndp[0][1]=check(b[0],1);
+    for(int i=1;i<m;++i)
+    {
+        ndp[i][0]=ndp[i-1][0]+check(b[i],-i-1);
+        for(int j=1;j<=i+1;++j)
         {
-            CountZ++;
-            for(int j=min(CountZ,m);~j;--j)
-            {
-                if(j>0)
-                {
-                    dp[j]=max(dp[j],dp[j-1]);
-                }
-            }
-        }
-        else
-        {
-            int temp=abs(a[i]);
-            if(a[i]>0)
-            {
-                for(int j=0;j<=min(CountZ,m);++j)
-                {
-                    int I=CountZ-j;
-                    if(I>=temp)dp[j]++;
-                }
-            }
-            else
-            {
-                for(int j=0;j<=min(CountZ,m);++j)
-                {
-                    int S=j;
-                    if(S>=temp)dp[j]++;
-                }
-            }
+            ndp[i][j]=max(ndp[i-1][j-1]+check(b[i],j),ndp[i-1][j]+check(b[i],j-i-1));
         }
     }
     int ans=0;
-    for(int i=0;i<=m;++i)ans=max(ans,dp[i]);
+    for(auto &it:ndp.back())
+    {
+        ans=max(ans,it);
+    }
     cout<<ans;
 }
 signed main()
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    cout.tie(0);
     sol();
     return 0;
 }
