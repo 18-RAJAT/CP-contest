287ea2cd009fd0fb11263e6f99e46dde36d45bcf
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-10-25 10:23:52 +0530

    25/10/24

--- D_Kousuke_s_Assignment.cpp
+++ D_Kousuke_s_Assignment.cpp
@@ -1,29 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-int segment(vector<int>&a,int n)
-{
-    unordered_map<int,int>mp;
-    int ans=0,sum=0;
-    mp[sum]=1;
-    for(int i=0;i<n;++i)
-    {
-        sum+=a[i];
-        if(mp[sum])
-        {
-            mp.clear(),sum=0,ans++;
-        }
-        mp[sum]=1;
-    }
-    return ans;
-}
 void sol()
 {
     int n;
     cin>>n;
     vector<int>a(n);
-    for(int i=0;i<n;++i)cin>>a[i];
-    cout<<segment(a,n)<<endl;
+    for(int i=0;i<n;++i)
+    {
+        cin>>a[i];
+        if(i>0)a[i]+=a[i-1];
+    }
+    map<int,int>mp;
+    int ans=0;
+    mp[0]=1;
+    for(int i=0;i<n;++i)
+    {
+        if(mp[a[i]]==1)
+        {
+            ans++,mp.clear();
+        }
+        mp[a[i]]=1;
+    }
+    cout<<ans<<endl;
 }
 signed main()
 {
