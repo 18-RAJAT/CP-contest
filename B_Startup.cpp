533f110df1bebf7f778df49a66bc57b5a0fa8814
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-11-03 17:47:59 +0530

    03/11/24

---
+++ B_Startup.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<int>a(k+5);
+    for(int i=0;i<k;++i)
+    {
+        int b,c;
+        cin>>b>>c;
+        a[b]+=c;
+    }
+    sort(a.begin(),a.end(),greater<int>());
+    int ans=0;
+    for(int i=0;i<min(n,k);++i)ans+=a[i];
+    cout<<ans<<endl;
+}
+signed main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        sol();
+    }
+    return 0;
+}
