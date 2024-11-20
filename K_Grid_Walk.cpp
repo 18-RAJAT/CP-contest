025bd76d86bdb5c4f0a53e2a8a47cacb679816f8
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-11-20 10:11:02 +0530

    20/11/24

--- K_Grid_Walk.cpp
+++ K_Grid_Walk.cpp
@@ -1,38 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-int Gcd(int a,int b)
+int gcd(int a,int b)
 {
-    if(b==0)return a;
-    return Gcd(b,a%b);
-}
-vector<int>compute(int n,int k)
-{
-    vector<int>ans(n+1,0);
-    for(int i=1;i<=n;++i)
-    {
-        ans[i]=Gcd(i,k);
-    }
-    return ans;
+    return b==0?a:gcd(b,a%b);
 }
 void sol()
 {
     int n,a,b;
     cin>>n>>a>>b;
-    vector<int>A=compute(n,a),B=compute(n,b);
-    vector<int>dp(n+1,0);
-    for(int i=1;i<=n;++i)
+    vector<int>ar(n+1),br(n+1);
+    for(int i=1;i<=n;++i)ar[i]=gcd(i,a);
+    for(int i=1;i<=n;++i)br[i]=gcd(i,b);
+    int x=-1,y=-1;
+    for(int i=n;i>0;--i)
     {
-        for(int j=1;j<=n;++j)
+        if(ar[i]==1 && x==-1)x=i;
+        if(br[i]==1 && y==-1)y=i;
+        if(~x && ~y)break;
+    }
+    vector<vector<int>>dp(n-x+1,vector<int>(n-y+1,1e18));
+    dp[0][0]=0;
+    for(int i=x;i<=n;++i)
+    {
+        for(int j=y;j<=n;++j)
         {
-            int cost=A[i]+B[j];
-            if(i==1 && j==1)dp[j]=cost;
-            else if(i==1)dp[j]=dp[j-1]+cost;
-            else if(j==1)dp[j]=dp[j]+cost;
-            else dp[j]=min(dp[j],dp[j-1])+cost;
+            if(i>x)dp[i-x][j-y]=min(dp[i-x][j-y],dp[i-x-1][j-y]);
+            if(j>y)dp[i-x][j-y]=min(dp[i-x][j-y],dp[i-x][j-y-1]);
+            dp[i-x][j-y]+=ar[i]+br[j];
         }
     }
-    cout<<dp[n]<<endl;
+    int ans=dp[n-x][n-y];
+    for(int i=1;i<=x;++i)ans+=ar[i]+1;
+    for(int i=1;i<=y;++i)ans+=br[i]+1;
+    ans-=4;
+    cout<<ans<<endl;
 }
 signed main()
 {
