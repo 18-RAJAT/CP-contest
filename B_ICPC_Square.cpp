044697b1a6d5e0c03b3a4a760c776be88ddca536
18-RAJAT <Rajatjoshi7535@gmail.com>
2024-12-02 09:40:20 +0530

    02/12/24

--- B_ICPC_Square.cpp
+++ B_ICPC_Square.cpp
@@ -1,28 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-
-unordered_map<int,int>memo;
-int n,d,s,cnt=0;
-int dfs(int x)
-{
-    if(memo.count(x))return memo[x];
-    if(cnt++>1e6)return x;
-    int ans=x,calc=min((d/x)+1,n/x);
-    for(int i=calc;i>=2;--i)
-    {
-        int y=x*i;
-        if(y>n)continue;
-        if(y-x>d)continue;
-        ans=max(ans,dfs(y));
-    }
-    memo[x]=ans;
-    return ans;
-}
 void sol()
 {
+    int n,d,s;
     cin>>n>>d>>s;
-    cout<<dfs(s)<<endl;
+    n/=s,d/=s;
+    if(n==1)
+    {
+        cout<<s;
+        return;
+    }
+    int ans=min(n,d+1);
+    for(int i=2;i*i<=n;++i)
+    {
+        int pos=i-1,mn=min(n/i,d/pos);
+        ans=max(i*mn,ans);
+    }
+    cout<<s*ans;
 }
 signed main()
 {
