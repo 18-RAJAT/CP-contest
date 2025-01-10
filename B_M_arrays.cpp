bebd491af1e736007f0abf202661a5214b81647a
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-01-10 07:37:03 +0530

    10/01/25

--- B_M_arrays.cpp
+++ B_M_arrays.cpp
@@ -1,43 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
-const int N=2e5+5;
-vector<int>adj[N];
 void sol()
 {
     int n,m;
     cin>>n>>m;
-    vector<int>a(n),visited(n);
+    vector<int>a(m);
     for(int i=0;i<n;++i)
     {
-        cin>>a[i];
-        a[i]%=m;
-        adj[a[i]].push_back(i);
+        int x;
+        cin>>x;
+        a[x%m]++;
     }
     int ans=0;
-    for(int i=0;i<n;++i)
+    if(a[0]!=0)ans++;
+    for(int i=1;i<m;++i)
     {
-        if(visited[i])
+        int b=m-i;
+        if(b<i)break;
+        if(i==b)
         {
-            continue;
+            if(a[i]!=0)ans++;
+            break;
         }
-        ans++;
-        int curr=i;
-        while(1)
-        {
-            // adj[a[curr]].erase(curr);
-            visited[curr]=1;
-            if(adj[(m-a[curr])%m].size()==0)
-            {
-                break;
-            }
-            curr=adj[(m-a[curr])%m].back();
-            // curr%=m;
-            // cout<<"current state-> "<<curr<<"\n";
-            adj[(m-a[curr])%m].pop_back();
-        }
+        if(a[i]==a[b] && a[i]==0)continue;
+        else if(a[i]==a[b])ans++;
+        else ans+=abs(a[i]-a[b]);
     }
-    cout<<ans<<"\n";
+    cout<<ans<<endl;
 }
 signed main()
 {
