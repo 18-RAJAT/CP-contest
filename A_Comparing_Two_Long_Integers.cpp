6e73b2eebf46e5512abdbd49b04dbb5063a2ffd4
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-01-31 13:02:11 +0530

    31/01/25

---
+++ A_Comparing_Two_Long_Integers.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+void sol()
+{
+    int a,b;
+    cin>>a>>b;
+    if(a>b)cout<<">";
+    else if(a<b)cout<<"<";
+    else cout<<"=";
+}
+signed main()
+{
+    sol();
+    return 0;
+}
