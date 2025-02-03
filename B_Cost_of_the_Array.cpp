93618348549cebb4259af967cf2e9ed9d8dd26fe
18-RAJAT <Rajatjoshi7535@gmail.com>
2025-02-03 09:18:36 +0530

    03/02/25

--- B_Cost_of_the_Array.cpp
+++ B_Cost_of_the_Array.cpp
@@ -5,67 +5,42 @@ void sol()
 {
     int n,k;
     cin>>n>>k;
-    vector<int>a(n),cnt(n+2,0),chk(n+2,0);
+    vector<int>a(n);
     for(int i=0;i<n;++i)cin>>a[i];
-    int res=-1;
-    for(int i=0;i<n;++i)
+    k=n-k+1;
+    if(k==1)
     {
-        int x=a[i];
-        if(x<=n+1)
+        int cnt=1;
+        for(int i=0;i<n;++i)
         {
-            cnt[x]++;
-            if(x!=res)chk[x]++;
-            // cout<<x<<" "<<cnt[x]<<" "<<chk[x]<<endl;
-            // else if(x==res && chk[x]==1)
-            // {
-            //     res=-1;
-            //     break;
-            // }
-            res=x;
-        }
-        else if(x==n+2)res=x;
-        else res=-1;
-    }
-    int ans=n+1;
-    for(int i=1;i<=n+1;++i)
-    {
-        if(cnt[i]==0)
-        {
-            ans=i;
-            break;
-        }
-        // else if(cnt[i]+k-chk[i]==n)
-        // {
-        //     ans=i;
-        //     break;
-        // }
-        else if(cnt[i]+k-chk[i]>n)
-        {
-            for(int j=i+1;j<=n+1;++j)
+            if(i&1)
             {
-                if(cnt[j]+k-chk[j]<=n)
+                if(cnt!=a[i])
                 {
-                    ans=j;
-                    break;
+                    cout<<cnt<<endl;
+                    return;
                 }
-            }
-            for(auto& it:cnt)
-            {
-                if(it+cnt[0]+k-chk[0]<=n)
-                {
-                    ans=0;
-                    break;
-                }
+                cnt++;
             }
         }
-        // for(auto& it:cnt)cout<<it<<" ";cout<<endl;
-        if(cnt[i]+k-chk[i]<=n || i==n+1)
-        {
-            ans=i;
-            break;
-        }
+        cout<<cnt<<endl;
+        return;
     }
-    cout<<ans<<endl;
+    else
+    {
+        int i=0,j=1;
+        while(i<k)
+        {
+            if(a[j]!=1)
+            {
+                cout<<1<<endl;
+                return;
+            }
+            i++,j++;
+        }
+        cout<<2<<endl;
+        return;
+    }
 }
 signed main()
 {
